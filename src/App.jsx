2bed9eb03d335415fca95df460743c2aa2d0db59
nguyen huy <quanghuy2371998@gmail.com>
2023-08-31 07:37:58 +0700

    ss5

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import BT1 from "./bt/BT1";
+import BT2 from "./bt/BT2";
+import BTbody1 from "./bt/BTbody1";
+import BTbody2 from "./bt/BTbody2";
+
+function App() {
+  return (
+    <>
+      {/* <BT1 />
+      <BTbody1 /> */}
+      <BT2 />
+      <BTbody2/>
+    </>
+  );
+}
+
+export default App;
